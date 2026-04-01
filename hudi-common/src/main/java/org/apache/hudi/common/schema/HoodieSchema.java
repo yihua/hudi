df68e019624ebd84dc9ea61a3be75459a38dcce0
Tim Brown <tim@generalintuition.com>
2026-03-31 20:36:08 -0400

    cleanup

--- hudi-common/src/main/java/org/apache/hudi/common/schema/HoodieSchema.java
+++ hudi-common/src/main/java/org/apache/hudi/common/schema/HoodieSchema.java
@@ -2570,14 +2570,6 @@ public class HoodieSchema implements Serializable {
       return REFERENCE_FIELD_COUNT;
     }
 
-    public static int getBlobReferenceFieldCount() {
-      return 4;
-    }
-
-    public static int getBlobFieldCount() {
-      return 3;
-    }
-
     /**
      * Creates a new HoodieSchema wrapping the given Avro schema.
      *
