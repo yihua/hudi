1fa19c74260e34170e4a587955c31da7c94f811b
Y Ethan Guo <ethan.guoyihua@gmail.com>
2026-09-05 14:47:18 -0700

    fix(client): keep small-file routing out of the update-as-delete-insert write mode
    Small-file handling routes merge-on-read updates through a merge handle and
    assigns inserts into existing file groups, which silently bypasses the
    positional-delete log path the write mode requires. With the mode enabled the
    delta commit executors always append updates and the upsert partitioners stop
    offering small files, so inserts land in base files of new file groups.

--- hudi-client/hudi-java-client/src/main/java/org/apache/hudi/table/action/deltacommit/BaseJavaDeltaCommitActionExecutor.java
+++ hudi-client/hudi-java-client/src/main/java/org/apache/hudi/table/action/deltacommit/BaseJavaDeltaCommitActionExecutor.java
@@ -74,7 +74,8 @@ abstract class BaseJavaDeltaCommitActionExecutor<T> extends BaseJavaCommitAction
                                                   long numUpdates,
                                                   Iterator<HoodieRecord<T>> recordItr) throws IOException {
     log.info("Merging updates for commit {} for file {}", instantTime, fileId);
-    if (!table.getIndex().canIndexLogFiles() && partitioner != null
+    if (!table.getIndex().canIndexLogFiles() && !config.shouldWriteUpdatesAsDeletesAndInserts()
+        && partitioner != null
         && partitioner.getSmallFileIds().contains(fileId)) {
       log.info("Small file corrections for updates for commit {} for file {}", instantTime, fileId);
       return super.handleUpdate(partitionPath, fileId, numUpdates, recordItr);
--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/table/action/deltacommit/BaseSparkDeltaCommitActionExecutor.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/table/action/deltacommit/BaseSparkDeltaCommitActionExecutor.java
@@ -73,7 +73,8 @@ public abstract class BaseSparkDeltaCommitActionExecutor<T>
                                                   long numUpdates,
                                                   Iterator<HoodieRecord<T>> recordItr) throws IOException {
     log.info("Merging updates for commit {} for file {}", instantTime, fileId);
-    if (!table.getIndex().canIndexLogFiles() && mergeOnReadUpsertPartitioner != null
+    if (!table.getIndex().canIndexLogFiles() && !config.shouldWriteUpdatesAsDeletesAndInserts()
+        && mergeOnReadUpsertPartitioner != null
         && mergeOnReadUpsertPartitioner.getSmallFileIds().contains(fileId)) {
       log.info("Small file corrections for updates for commit {} for file {}", instantTime, fileId);
       return super.handleUpdate(partitionPath, fileId, numUpdates, recordItr);
